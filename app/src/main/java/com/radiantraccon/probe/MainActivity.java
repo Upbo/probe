5ac843c13855a293a0620047a45d5d020c68a08f
upbo <khd1323@naver.com>
2019-09-10 23:01:57 +0900

    Add permission request

--- app/src/main/java/com/radiantraccon/probe/MainActivity.java
+++ app/src/main/java/com/radiantraccon/probe/MainActivity.java
@@ -1,14 +1,49 @@
 package com.radiantraccon.probe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] permissionList = {
+            Manifest.permission.INTERNET
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!hasPermissions()) {
+            requestPermissions(permissionList, 0);
+        }
+    }
+
+    private boolean hasPermissions() {
+        for (String p : permissionList) {
+            int check = checkCallingOrSelfPermission(p);
+            return check != PackageManager.PERMISSION_DENIED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 0){
+            for(int i=0; i<grantResults.length; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // if permission granted
+                }
+                else {
+                    // if permission denied,
+                }
+            }
+        }
     }
 }
