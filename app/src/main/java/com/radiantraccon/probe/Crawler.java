606c4e62c0b35c73c5695b0579184b9f38da4d43
upbo <khd1326@naver.com>
2019-09-21 19:58:12 +0900

    Add Crawler class to get html data

---
+++ app/src/main/java/com/radiantraccon/probe/Crawler.java
@@ -0,0 +1,22 @@
+package com.radiantraccon.probe;
+
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+
+public class Crawler extends AsyncTask<String, Integer, ArrayList<KeywordData>> {
+    @Override
+    protected void onPostExecute(ArrayList<KeywordData> keywordData) {
+        super.onPostExecute(keywordData);
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected ArrayList<KeywordData> doInBackground(String... strings) {
+        return null;
+    }
+}
