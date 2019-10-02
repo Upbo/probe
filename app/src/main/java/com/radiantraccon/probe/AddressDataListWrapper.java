d2580843e5b7a858758b98da4eb37f1ca437fc4e
upbo <khd1323@naver.com>
2019-10-02 23:26:23 +0900

    Implement AddressData wrapper class

--- app/src/main/java/com/radiantraccon/probe/AddressDataListWrapper.java
+++ app/src/main/java/com/radiantraccon/probe/AddressDataListWrapper.java
@@ -3,8 +3,11 @@ package com.radiantraccon.probe;
 import java.util.ArrayList;
 
 public class AddressDataListWrapper {
-    private ArrayList<AddressData> addressDataList = new ArrayList<>(20);
-    private AddressAdapter addressAdapter;
 
+    private ArrayList<AddressData> addressDataList;
+    private AddressAdapter addressAdapter;
+    public AddressDataListWrapper(int intialCapacity) {
+        addressDataList = new ArrayList<>(intialCapacity);
+    }
 
 }
