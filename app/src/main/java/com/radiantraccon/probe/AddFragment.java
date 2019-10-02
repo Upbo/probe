d71502e26dfcac19a503dc24a68151902f0638a0
upbo <khd1323@naver.com>
2019-10-02 23:28:24 +0900

    Implement Addfragment for adding user data(address, keyword)

--- app/src/main/java/com/radiantraccon/probe/AddFragment.java
+++ app/src/main/java/com/radiantraccon/probe/AddFragment.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 /**
@@ -30,7 +32,19 @@ public class AddFragment extends Fragment {
         // TODO: Initialize RecyclerView of web page addresses
 
         final EditText keywordEditText = view.findViewById(R.id.editText_keyword);
+        final AddressData addressData;
         Button submitButton = view.findViewById(R.id.button_submit);
+        Button addressButton = view.findViewById(R.id.button_address);
+        addressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddressFragment addressFragment = new AddressFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.frameLayout, addressFragment);
+                fragmentTransaction.commit();
+            }
+        });
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
