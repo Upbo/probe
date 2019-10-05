551d8e91a69db171315503792d1c3ff7a075a870
upbo <khd1323@naver.com>
2019-10-05 23:55:35 +0900

    Implement EventDialog to show information

--- app/src/main/java/com/radiantraccon/probe/EventDialogFragment.java
+++ app/src/main/java/com/radiantraccon/probe/EventDialogFragment.java
@@ -1,6 +1,39 @@
 package com.radiantraccon.probe;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.DialogFragment;
 
 public class EventDialogFragment extends DialogFragment {
+
+    public EventDialogFragment() { }
+    public static EventDialogFragment newInstance(String text) {
+        EventDialogFragment fragment = new EventDialogFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("text", text);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_event, container);
+        Button button = view.findViewById(R.id.button_event);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return view;
+    }
 }
