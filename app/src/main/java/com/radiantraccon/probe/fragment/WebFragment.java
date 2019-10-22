02f7a3819b88bea6512eca39cc98e5c4f712a83e
upbo <khd1326@naver.com>
2019-10-22 18:33:36 +0900

    Make action bar in WebFragment invisible

--- app/src/main/java/com/radiantraccon/probe/fragment/WebFragment.java
+++ app/src/main/java/com/radiantraccon/probe/fragment/WebFragment.java
@@ -1,9 +1,14 @@
 package com.radiantraccon.probe.fragment;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,13 +34,24 @@ public class WebFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_web, container, false);
         // Inflate the layout for this fragment
         Bundle bundle = getArguments();
         if(bundle != null) {
             url = bundle.getString("URL");
             Log.e("WebView", url);
         }
-        View view = inflater.inflate(R.layout.fragment_web, container, false);
+
+        // Hide toolbar and navigation bar in WebFragment
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                Navigation.findNavController(view).navigateUp();
+                ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
         WebView webView = view.findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
