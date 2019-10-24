91b463c854dcdeda1f03e57141e832dcb96829b9
upbo <khd1326@naver.com>
2019-10-24 12:35:02 +0900

    Remove unused function

--- app/src/main/java/com/radiantraccon/probe/CrawlOption.java
+++ app/src/main/java/com/radiantraccon/probe/CrawlOption.java
@@ -3,6 +3,8 @@ package com.radiantraccon.probe;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import androidx.preference.PreferenceManager;
+
 public class CrawlOption {
     public static int pagesPerCrawl;
     public static float version = 0.1f;
@@ -10,16 +12,10 @@ public class CrawlOption {
     public CrawlOption() { }
 
     public static void loadPreferences(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.preferences_filename), Context.MODE_PRIVATE);
+        String filename = context.getPackageName() + "_preferences";
+        // PreferenceManager?
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(filename, Context.MODE_PRIVATE);
         pagesPerCrawl = sharedPreferences.getInt("pagesPerCrawl", 5);
-    }
-
-    public static void savePagesPerCrawl(Context context, int page) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.preferences_filename), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt("pagesPerCrawl", page);
-        editor.commit();
-
-        pagesPerCrawl = page;
     }
 }
