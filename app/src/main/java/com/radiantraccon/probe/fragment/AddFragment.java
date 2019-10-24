13a89e9d35c17ccb4da7d43266a84b192287ae9a
upbo <khd1326@naver.com>
2019-10-24 09:22:13 +0900

    Add EditorActionListener to EditText for UX

--- app/src/main/java/com/radiantraccon/probe/fragment/AddFragment.java
+++ app/src/main/java/com/radiantraccon/probe/fragment/AddFragment.java
@@ -2,13 +2,20 @@ package com.radiantraccon.probe.fragment;
 
 
 import android.content.Context;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -23,6 +30,8 @@ public class AddFragment extends Fragment {
     private int imageId;
     private String title;
     private String address;
+    private String keywordText;
+    private String descText;
 
     public AddFragment() {
         // Required empty public constructor
@@ -37,8 +46,8 @@ public class AddFragment extends Fragment {
 
         final EditText keywordEditText = view.findViewById(R.id.editText_keyword);
         final EditText descEditText = view.findViewById(R.id.editText_desc);
-        Button submitButton = view.findViewById(R.id.button_submit);
-        Button addressButton = view.findViewById(R.id.button_address);
+        final Button submitButton = view.findViewById(R.id.button_submit);
+        final Button addressButton = view.findViewById(R.id.button_address);
 
         Bundle bundle = getArguments();
         if(bundle != null) {
@@ -46,14 +55,23 @@ public class AddFragment extends Fragment {
             title = bundle.getString("title");
             address = bundle.getString("address");
             addressButton.setText(title);
+
+            keywordText = bundle.getString("keywordText");
+            descText = bundle.getString("descText");
+            keywordEditText.setText(keywordText);
+            descEditText.setText(descText);
         }
 
         addressButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_addFragment_to_addressFragment);
+                Bundle bundle = new Bundle();
+                bundle.putString("keywordText", keywordEditText.getText().toString());
+                bundle.putString("descText", descEditText.getText().toString());
+                Navigation.findNavController(view).navigate(R.id.action_addFragment_to_addressFragment, bundle);
             }
         });
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +92,24 @@ public class AddFragment extends Fragment {
                             .show();
 
                 }
+            }
+        });
+
+        descEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch(actionId) {
+                    case EditorInfo.IME_ACTION_DONE:
+                        InputMethodManager inputMethodManager = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                        if(address != null){
+                            submitButton.performClick();
+                        } else {
+                            addressButton.performClick();
+                        }
+                        return true;
+                }
+                return false;
             }
         });
         return view;
--- app/src/main/java/com/radiantraccon/probe/fragment/AddressFragment.java
+++ app/src/main/java/com/radiantraccon/probe/fragment/AddressFragment.java
@@ -2,6 +2,7 @@ package com.radiantraccon.probe.fragment;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,8 @@ import com.radiantraccon.probe.data.AddressDataListWrapper;
  * A simple {@link Fragment} subclass.
  */
 public class AddressFragment extends Fragment {
-
+    private String keywordText;
+    private String descText;
     public AddressDataListWrapper addresses;
     public AddressFragment() {
         // Required empty public constructor
@@ -39,9 +41,15 @@ public class AddressFragment extends Fragment {
         recyclerView.setLayoutManager(linearLayoutManager);
         addresses.readAddressDataFile(getString(R.string.addressData_filename), getContext());
 
+        Bundle bundle = getArguments();
+        if(bundle != null) {
+            Log.e("AddressFrag", keywordText + " " +descText);
+            keywordText = bundle.getString("keywordText");
+            descText = bundle.getString("descText");
+        }
+
         addresses.initAdapter();
         final AddressAdapter adapter = addresses.getAddressAdapter();
-        // TODO: BUG item shows twice
         recyclerView.setAdapter(adapter);
         adapter.setOnItemListener(new AddressAdapter.OnItemClickListener() {
             @Override
@@ -51,6 +59,8 @@ public class AddressFragment extends Fragment {
                 bundle.putInt("imageId", data.getImageId());
                 bundle.putString("title", data.getTitle());
                 bundle.putString("address", data.getAddress());
+                bundle.putString("keywordText", keywordText);
+                bundle.putString("descText", descText);
                 Navigation.findNavController(view).navigate(R.id.action_addressFragment_to_addFragment, bundle);
             }
         });
